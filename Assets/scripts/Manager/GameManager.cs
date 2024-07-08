e3c997131597448794de877f4b62a0df9a6b5439
ChangHyeon Kim <hazasue@gmail.com>
2024-07-08 17:24:17 +0900

    :hammer: Create/Load Game File

--- Assets/scripts/Manager/GameManager.cs
+++ Assets/scripts/Manager/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : Singleton<GameManager>
     private const float DEFAULT_TIME_SCALE_PAUSED = 0f;
     private const float DEFAULT_TIME_SCALE_PLAYING = 1f;
 
+    private Dictionary<int, GameData> gameDatas;
     private Dictionary<int, List<NpcData>> npcDatas;
 
     //플레이 타임 변수
@@ -59,9 +60,12 @@ public class GameManager : Singleton<GameManager>
             npcDatas = JsonManager.LoadJsonFile<Dictionary<int, List<NpcData>>>(JsonManager.DEFAULT_NPC_DATA_NAME);
         }
 
-        initNpcTrait();
-
-
+        if (JsonManager.LoadJsonFile<CurrentGameInfo>(JsonManager.DEFAULT_CURRENT_DATA_NAME).newGame)
+            newGame();
+        else
+        {
+            loadGame(JsonManager.LoadJsonFile<CurrentGameInfo>(JsonManager.DEFAULT_CURRENT_DATA_NAME).dataId);
+        }
     }
 
     public void PauseGame()
@@ -78,8 +82,42 @@ public class GameManager : Singleton<GameManager>
         Debug.Log("Game Playing");
     }
 
+    private void newGame()
+    {
+        if (!File.Exists(Application.dataPath + "/Data/" + JsonManager.DEFAULT_GAME_DATA_NAME + ".json"))
+        {
+            gameDatas = new Dictionary<int, GameData>();
+            JsonManager.CreateJsonFile(JsonManager.DEFAULT_GAME_DATA_NAME, gameDatas);
+        }
+        else
+        {
+            gameDatas = JsonManager.LoadJsonFile<Dictionary<int, GameData>>(JsonManager.DEFAULT_GAME_DATA_NAME);
+        }
+
+        int currentKey = 0;
+        
+        if(gameDatas.Count <= 0) 
+            gameDatas.Add(0, new GameData("test", 0, 0, 0, 0f, initNpcTrait()));
+        else
+        {
+            foreach (int data in gameDatas.Keys)
+            {
+                if (data > currentKey) currentKey = data;
+            }
+            
+            gameDatas.Add(currentKey + 1, new GameData("test", 0, 0, 0, 0f, initNpcTrait()));
+        }
+
+        JsonManager.CreateJsonFile(JsonManager.DEFAULT_GAME_DATA_NAME, gameDatas);
+    }
+
+    private void loadGame(int dataId)
+    {
+        
+    }
+
     // init npc trait when new game starts
-    private void initNpcTrait()
+    private List<NpcData> initNpcTrait()
     {
         List<NpcData> npcInitDatas = JsonManager.LoadJsonFile<List<NpcData>>(JsonManager.DEFAULT_NPCINIT_DATA_NAME);
 
@@ -95,6 +133,8 @@ public class GameManager : Singleton<GameManager>
             Debug.Log(
                 $"{data.id}, {data.name}, {trait}, {data.traitName1}, {data.traitEvents1[0]}, {data.traitEvents1[1]}, {!trait}, {data.traitName2}, {data.traitEvents2[0]}, {data.traitEvents2[1]}");
         }
+
+        return tempNpcDatas;
 
 
         // Add tempNpcDatas into npcDatas and save json file
