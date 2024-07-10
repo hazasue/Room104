a4c9b5cbf2564e6f85aae3d37b21e69dc1bf2414
ChangHyeon Kim <hazasue@gmail.com>
2024-07-10 13:08:43 +0900

    :hammer: Show saved file infos in one text

--- Assets/scripts/Model/LoadGameInfo.cs
+++ Assets/scripts/Model/LoadGameInfo.cs
@@ -7,14 +7,19 @@ public class LoadGameInfo : MonoBehaviour
 {
     public TMP_Text rootTypeText;
     public TMP_Text episodeText;
-    public TMP_Text dateText;
-    public TMP_Text playTimeText;
+    public TMP_Text infoText;
 
     public void Init(string rootType, int episode, int date, int gameTime, float playTime)
     {
         rootTypeText.text = rootType;
         episodeText.text = $"{episode}";
-        dateText.text = $"{date} 일차 {gameTime}";
-        playTimeText.text = $"플레이 타임: {playTime.ToString("F2")}";
+        infoText.text = $"{date} 일차 {gameTime}\n플레이 타임: {playTime.ToString("F2")}";
+    }
+
+    public void Init(GameData gameData)
+    {
+        rootTypeText.text = gameData.root;
+        episodeText.text = $"{gameData.episode}";
+        infoText.text = $"{gameData.date} 일차 {gameData.gameTime}\n플레이 타임: {gameData.playTime.ToString("F2")}";
     }
 }
