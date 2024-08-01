64f8a5cb6e754d011a66632b9fdd6fb30b4b8d13
ChangHyeon Kim <hazasue@gmail.com>
2024-08-01 13:08:57 +0900

    :hammer: Save say talk datas

--- Assets/scripts/Model/SayTalk.cs
+++ Assets/scripts/Model/SayTalk.cs
@@ -4,14 +4,116 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+public enum eSayTalkType
+{
+    NARRATIVE,
+    SFX,
+    OBJECT,
+    TEXT,
+    ANIMATION
+}
+
 public class SayTalk : MonoBehaviour
 {
-    public TMP_Text target;
-    public TMP_Text content;
-
-    public void Init(int target, string content)
+    private int num;
+    public int Num
     {
-        this.target.text = $"{target}";
-        this.content.text = $"{content}";
+        get { return num; }
+    }
+    
+    private eSayTalkType type;
+    public eSayTalkType Type 
+    { 
+        get { return type; }
+    }
+
+    private bool isPlayer;
+    public bool IsPlayer
+    {
+        get { return isPlayer; }
+    }
+
+    private bool autoSkip;
+    public bool AutoSkip
+    {
+        get { return autoSkip; }
+    }
+    
+    private string text;
+    public string Text
+    {
+        get { return text; }
+    }
+    
+    private float duration;
+    public float Duration
+    {
+        get { return duration; }
+    }
+
+    private bool loop;
+    public bool Loop
+    {
+        get { return loop; }
+    }
+
+    private Color32 color;
+    public Color32 Color
+    {
+        get { return color; }
+    }
+
+    private string direction;
+    public string Direction
+    {
+        get { return direction; }
+    }
+
+    private int target;
+
+    public int Target
+    {
+        get { return target; }
+    }
+
+    public SayTalk(int num, string type, bool isPlayer, bool autoSkip, string text, float duration, bool loop,
+        Color32 color, string direction, int target)
+    {
+        this.num = num;
+        this.isPlayer = isPlayer;
+        this.autoSkip = autoSkip;
+        this.text = text;
+        this.duration = duration;
+        this.loop = loop;
+        this.color = color;
+        this.direction = direction;
+        this.target = target;
+
+        switch (type)
+        {
+            case "narrative":
+                this.type = eSayTalkType.NARRATIVE;
+                break;
+            
+            case "sfx":
+                this.type = eSayTalkType.SFX;
+                break;
+            
+            case "object":
+                this.type = eSayTalkType.OBJECT;
+                break;
+            
+            case "text":
+                this.type = eSayTalkType.TEXT;
+                break;
+            
+            case "animation":
+                this.type = eSayTalkType.ANIMATION;
+                break;
+            
+            default:
+                Debug.Log($"Invalid say talk type: {type}");
+                break;
+        }
     }
 }
