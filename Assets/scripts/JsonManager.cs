a72825e0fc10fca10c175c46953aeda2a4384555
ChangHyeon Kim <hazasue@gmail.com>
2024-08-16 17:23:25 +0900

    :wrench: Add file name

--- Assets/scripts/JsonManager.cs
+++ Assets/scripts/JsonManager.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class JsonManager
 {
     public static string DEFAULT_SETTING_DATA_NAME = "settings";
+    public static string DEFAULT_PRODUCTINIT_DATA_NAME = "products_init";
     public static string DEFAULT_PRODUCT_DATA_NAME = "products";
     public static string DEFAULT_NPCINIT_DATA_NAME = "npcinitdata";
     public static string DEFAULT_NPC_DATA_NAME = "npcdata";
