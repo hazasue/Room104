c67a1592d982cce11825c35b2de3cb4c31a9ae3b
ChangHyeon Kim <hazasue@gmail.com>
2024-08-19 18:30:04 +0900

    :tada: Init AchievementManager

---
+++ Assets/scripts/Manager/AchievementManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+    private Dictionary<int, Achievement> achievements;
+
+    private void Awake()
+    {
+        init();
+    }
+
+    private void init()
+    {
+        //parsing from achievement table
+        // init achievement dynamically
+    }
+}                                                                                                           
