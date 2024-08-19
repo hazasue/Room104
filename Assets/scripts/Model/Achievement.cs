e45f5584bcf1b85afca08fba447d84b009e18bd7
ChangHyeon Kim <hazasue@gmail.com>
2024-08-19 18:30:12 +0900

    :tada: Init Achievement script

---
+++ Assets/scripts/Model/Achievement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievement : MonoBehaviour
+{
+    private int id;
+    private string imagePath;
+    private string title;
+    private string description;
+    private string hint;
+    private bool condition;
+    private int currentFigure;
+    private int maxFigure;
+
+    public void Init(int id, string imagePath, string title, string description, string hint, bool condition, int currentFigure, int maxFigure)
+    {
+        
+    }
+
+}
